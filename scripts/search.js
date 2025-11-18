003eb3d00aae24711579f4225e483c65cf94e46f
Votre Nom <votre@email.com>
2025-11-18 15:44:04 +0100

    ajout de la branch christ-branch

---
+++ scripts/search.js
@@ -0,0 +1,21 @@
+
+const lis = document.querySelectorAll("ul li");  
+  lis.forEach(li => {
+    let bool = true
+
+  li.addEventListener("click", (event) => {
+      const couleurs = ["red", "yellow", "purple", "green"];
+      if (bool) {
+        li.style.backgroundColor = "red";
+        console.log("DTFGHFH");
+        bool = false;
+      } else {
+        li.style.backgroundColor = "blue";
+        bool = true;
+      }
+
+
+    }
+  );
+});
+
